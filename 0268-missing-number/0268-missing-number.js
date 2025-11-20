fbfd94531ddeb546eccaa417c96574e855bef1a3
anshifariya042 <anshifariya042@gmail.com>
2025-11-20 10:21:31 +0530

    Time: 0 ms (100%), Space: 57.6 MB (38.69%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let n=nums.length;
+    let expected=n *(n+1)/2;
+    let result=nums.reduce((a,b)=>a+b,0);
+    return expected-result;
+};
+console.log(missingNumber([3,0,1]))
