6b94b427bdf7ff70c23374e7465b4ed3ef72aea2
anshifariya042 <anshifariya042@gmail.com>
2025-11-24 11:41:43 +0530

    Time: 44 ms (64.98%), Space: 53.2 MB (92%) - LeetHub

---
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -0,0 +1,12 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+console.log(argumentsLength ([5]));
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
